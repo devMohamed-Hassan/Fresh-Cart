46f31a99aa814a1a4b8e5f25b15301dd07fa16ab
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2025-07-05 02:02:07 +0300

    style(navbar): update UI

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,45 +3,37 @@ import { NavLink, useNavigate, Link } from "react-router-dom";
 import logo from "../../assets/images/freshcart-logo.svg";
 import { userContext } from "../../Context/UserContext";
 import { CartContext } from "../../Context/CartContext";
+import useClickOutside from "../../Hooks/useClick";
 
 function Navbar() {
   const { token, setToken } = useContext(userContext);
   const { cart } = useContext(CartContext);
   const navigate = useNavigate();
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isAvatarMenuOpen, setIsAvatarMenuOpen] = useState(false);
   const avatarRef = useRef(null);
+  const menuRef = useRef(null);
+  const totalItems = cart?.products?.reduce((acc, item) => acc + item.count, 0) || 0;
+  const wishlistCount = 1;
+  const username = localStorage.getItem("userName") || "User";
 
-  const totalItems = cart.products.reduce((acc, item) => acc + item.count, 0);
-  const wishlistCount = 0;
-  const username = localStorage.getItem("userName");
+  useClickOutside(menuRef, () => setIsMenuOpen(false), isMenuOpen);
+  useClickOutside(avatarRef, () => setIsAvatarMenuOpen(false), isAvatarMenuOpen);
 
   const handleSignout = () => {
     localStorage.removeItem("userToken");
-    localStorage.removeItem("userName")
+    localStorage.removeItem("userName");
     setToken("");
     navigate("/");
   };
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (avatarRef.current && !avatarRef.current.contains(event.target)) {
-        setIsAvatarMenuOpen(false);
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
   const navLinkClasses = ({ isActive }) =>
-    `block py-1 text-sm font-semibold transition-colors duration-200 border-b-2 ${isActive
-      ? "text-green-600 border-[var(--main-color)]"
-      : "text-gray-700 border-transparent hover:text-green-600 hover:border-green-600"
+    `block py-1 text-sm font-semibold transition-colors duration-200 
+   border-b-0 sm:border-b-2
+   ${isActive
+      ? "text-green-600 sm:border-[var(--main-color)]"
+      : "text-gray-700 border-transparent hover:text-green-600 hover:sm:border-green-600"
     }`;
-
 
   return (
     <nav className="bg-white border-b border-gray-200 sticky top-0 z-50 shadow-sm">
@@ -55,6 +47,11 @@ function Navbar() {
           <NavLink to="/products" className={navLinkClasses}>Products</NavLink>
           <NavLink to="/categories" className={navLinkClasses}>Categories</NavLink>
           <NavLink to="/brands" className={navLinkClasses}>Brands</NavLink>
+          {!token && (
+            <NavLink to="/login" className={navLinkClasses}>
+              Login
+            </NavLink>
+          )}
         </div>
 
         <div className="hidden md:flex items-center gap-6">
@@ -63,7 +60,7 @@ function Navbar() {
               <Link to="/wishlist" className="relative text-gray-600 hover:text-pink-500">
                 <i className="fa-regular fa-heart text-xl"></i>
                 {wishlistCount > 0 && (
-                  <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full shadow-sm">
+                  <span className="absolute -top-2 -right-2 bg-green-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full shadow-sm">
                     {wishlistCount}
                   </span>
                 )}
@@ -72,7 +69,7 @@ function Navbar() {
               <Link to="/cart" className="relative text-gray-600 hover:text-green-600">
                 <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M17 18a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2M1 2h3.27l.94 2H20a1 1 0 0 1 1 1c0 .17-.05.34-.12.5l-3.58 6.47c-.34.61-1 1.03-1.75 1.03H8.1l-.9 1.63l-.03.12a.25.25 0 0 0 .25.25H19v2H7a2 2 0 0 1-2-2c0-.35.09-.68.24-.96l1.36-2.45L3 4H1zm6 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2m9-7l2.78-5H6.14l2.36 5z"></path></svg>
                 {totalItems > 0 && (
-                  <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full shadow-sm">
+                  <span className="absolute -top-2 -right-2 bg-green-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full shadow-sm">
                     {totalItems}
                   </span>
                 )}
@@ -80,32 +77,35 @@ function Navbar() {
             </>
           )}
 
-          {!token ? (
-            <NavLink to="/login" className={navLinkClasses}>
-              Login
-            </NavLink>
-          ) : (
+          {token && (
             <div className="relative flex items-center gap-2 px-2" ref={avatarRef}>
-              <span className="text-sm font-semibold text-gray-500 truncate max-w-[100px]">
-                {username || "User"}
+              <span className="text-sm font-semibold text-gray-500 truncate max-w-[140px]">
+                Welcome, {username || "User"}
               </span>
               <button
                 onClick={() => setIsAvatarMenuOpen(!isAvatarMenuOpen)}
                 className="w-9 h-9 flex items-center justify-center rounded-full hover:bg-gray-200 transition-colors duration-200"
               >
-                <i className="fa-solid fa-user text-md text-red-900 rounded"></i>
+                <i className="fa-solid fa-circle-user text-3xl text-green-600 rounded"></i>
               </button>
 
               {isAvatarMenuOpen && (
-                <div className="absolute right-0 top-12 w-44  mt-1 shadow-lg z-50">
+                <div className="absolute right-0 top-12 w-45 mt-1 bg-white shadow-lg rounded-md z-50 overflow-hidden">
+
+                  <div className="px-4 py-3 bg-gray-100 border-b border-gray-300">
+                    <p className="text-sm text-gray-700 font-medium">
+                      <span className="block font-semibold">Welcome,</span><span className="text-gray-500 font-semibold">{username || "User"}</span>
+                    </p>
+                  </div>
+
                   <button
                     onClick={() => {
                       navigate("/change-password");
                       setIsAvatarMenuOpen(false);
                     }}
-                    className="flex items-center gap-2 w-full text-sm bg-red-500  text-white px-4 py-2 hover:bg-red-600 transition-colors duration-200"
+                    className="flex items-center gap-2 w-full text-sm text-gray-800 px-4 py-2 font-semibold hover:bg-gray-100 transition-colors duration-200"
                   >
-                    <i className="fa-solid fa-key text-base"></i>
+                    <i className="fa-solid fa-key text-base text-red-500"></i>
                     Change Password
                   </button>
 
@@ -114,7 +114,7 @@ function Navbar() {
                       handleSignout();
                       setIsAvatarMenuOpen(false);
                     }}
-                    className="flex items-center gap-2 w-full text-sm bg-red-500 text-white px-4 py-2 hover:bg-red-600 transition-colors duration-200"
+                    className="flex items-center gap-2 w-full text-sm bg-red-600 font-semibold text-white px-4 py-2 hover:bg-red-800 transition-colors duration-200"
                   >
                     <i className="fa-solid fa-right-from-bracket text-base"></i>
                     Log Out
@@ -124,6 +124,7 @@ function Navbar() {
 
             </div>
           )}
+
         </div>
 
         <div className="md:hidden">
@@ -139,34 +140,76 @@ function Navbar() {
       </div>
 
       {isMenuOpen && (
-        <div className="md:hidden px-4 pb-4 space-y-1 bg-white shadow-md rounded-b-lg">
-          <NavLink to="/" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Home</NavLink>
-          <NavLink to="/products" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Products</NavLink>
-          <NavLink to="/categories" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Categories</NavLink>
-          <NavLink to="/brands" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Brands</NavLink>
+        <div
+          ref={menuRef}
+
+          className="md:hidden px-4 pb-4 pt-3 space-y-2 bg-white shadow-md rounded-b-lg border-t border-gray-200">
 
           {token && (
-            <>
-              <NavLink to="/wishlist" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Wishlist</NavLink>
-              <NavLink to="/cart" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Cart</NavLink>
-            </>
+            <div className="flex items-center  bg-gray-100 p-2 justify-between pb-2 border-b border-gray-200 rounded-sm">
+              <p className="text-sm text-gray-600 font-medium">
+                <span className="text-gray-800 font-semibold">Welcome, </span>
+                <span className="text-gray-400 font-semibold">{username || "User"}</span>
+              </p>
+            </div>
           )}
+          <div className="bg-gray-100 w-full p-2 rounded-sm">
+            <NavLink to="/" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Home</NavLink>
+            <NavLink to="/products" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Products</NavLink>
+            <NavLink to="/categories" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Categories</NavLink>
+            <NavLink to="/brands" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Brands</NavLink>
 
-          {!token ? (
-            <NavLink to="/login" className={navLinkClasses} onClick={() => setIsMenuOpen(false)}>Login</NavLink>
-          ) : (
-            <button
-              onClick={() => {
-                handleSignout();
-                setIsMenuOpen(false);
-              }}
-              className="block w-full text-left text-red-600 hover:bg-red-50 text-sm px-4 py-2 rounded-md"
-            >
-              Sign Out
-            </button>
+          </div>
+
+
+          {token && (
+            <div className="flex items-center  pt-3 border-t border-gray-500">
+              <button
+                onClick={() => {
+                  handleSignout();
+                  setIsMenuOpen(prev => !prev);
+                }}
+                className="w-3/2 flex items-center justify-center gap-2 text-sm px-3 py-2 bg-red-500 text-white font-semibold hover:bg-red-600 rounded-md transition"
+              >
+                <i className="fa-solid fa-right-from-bracket"></i>
+                Log Out
+              </button>
+
+              <NavLink
+                to="/wishlist"
+                className="w-1/4 flex justify-center text-red-500 text-lg hover:text-red-600 transition relative"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <i className="fa-regular fa-heart text-xl "></i>
+                {wishlistCount > 0 && (
+                  <span className="absolute -top-2 -right-2 bg-green-500 font-bold text-white text-xs w-5 h-5 flex items-center justify-center rounded-full shadow-sm">
+                    {wishlistCount}
+                  </span>
+                )}
+
+              </NavLink>
+
+              <NavLink
+                to="/cart"
+                className="w-1/4 flex justify-center text-green-600 text-lg hover:text-green-700 transition relative"
+                onClick={() => setIsMenuOpen(false)}
+              >
+
+                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M17 18a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2M1 2h3.27l.94 2H20a1 1 0 0 1 1 1c0 .17-.05.34-.12.5l-3.58 6.47c-.34.61-1 1.03-1.75 1.03H8.1l-.9 1.63l-.03.12a.25.25 0 0 0 .25.25H19v2H7a2 2 0 0 1-2-2c0-.35.09-.68.24-.96l1.36-2.45L3 4H1zm6 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2m9-7l2.78-5H6.14l2.36 5z"></path></svg>
+                {totalItems > 0 && (
+                  <span className="absolute -top-2 -right-2 bg-green-500 font-bold text-white text-xs w-5 h-5 flex items-center justify-center rounded-full shadow-sm">
+                    {totalItems}
+                  </span>
+                )}
+              </NavLink>
+
+            </div>
           )}
         </div>
       )}
+
+
+
     </nav>
   );
 }
